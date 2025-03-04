8798f13ab8252487ac40d3e6c5273bdd415de1c3
henryhoangduong <henryhoangduong@gmail.com>
2025-03-04 15:00:10 +0700

    add ruru

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
-import {graphql, buildSchema} from "graphql"
-
+var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var {ruruHTML} = require("ruru/server")
 var schema = buildSchema(
-    `type Query{
-        hello: String
-    }`
-)
+  `type Query{
+        hello: String,
+        age:Int
+    }`,
+);
 
 var rootValue = {
-    hello: () => {
-        return "Hello world";
-    }
-}
+  hello: () => {
+    return "Hello world";
+  },
+  age: () => {
+    return 25;
+  },
+};
 
 graphql({
-    schema,
-    source: "{hello}",
-    rootValue,
-}).then(response => console.log(response))
+  schema,
+  source: "{hello}",
+  rootValue,
+}).then((response) => console.log(response));
+
+const app = express();
+
+app.get("/", (_req, res) => {
+    res.type("html");
+    res.end(ruruHTML({endpoint:"/graphql"}))
+})
+
+app.all("/graphql", createHandler({ schema: schema, rootValue: rootValue }));
+app.listen(4000);
+console.log("Api running on: http://localhost:4000");
