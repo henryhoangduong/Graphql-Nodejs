8f7e4799413326b26436a534f9df7a7aeead7f65
henryhoangduong <henryhoangduong@gmail.com>
2025-03-04 15:34:26 +0700

    install yoga

--- server.js
+++ server.js
@@ -1,36 +1,25 @@
-var { graphql, buildSchema } = require("graphql");
-var express = require("express");
-var { createHandler } = require("graphql-http/lib/use/express");
-var {ruruHTML} = require("ruru/server")
-var schema = buildSchema(
-  `type Query{
-        hello: String,
-        age:Int
-    }`,
-);
+import express from "express";
+import { ruruHTML } from "ruru/server";
+import { createSchema, createYoga } from "graphql-yoga";
 
-var rootValue = {
-  hello: () => {
-    return "Hello world";
-  },
-  age: () => {
-    return 25;
-  },
-};
-
-graphql({
-  schema,
-  source: "{hello}",
-  rootValue,
-}).then((response) => console.log(response));
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: `
+            type Query{
+            hello:String
+        }
+        `,
+    resolvers: {
+      Query: {
+        hello: () => "hello from yoga",
+      },
+    },
+  }),
+});
 
 const app = express();
 
-app.get("/", (_req, res) => {
-    res.type("html");
-    res.end(ruruHTML({endpoint:"/graphql"}))
-})
+app.get("/",yoga);
 
-app.all("/graphql", createHandler({ schema: schema, rootValue: rootValue }));
 app.listen(4000);
 console.log("Api running on: http://localhost:4000");
